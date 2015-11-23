df65767f7b043d81fdafdf0be6c0415b4cbbead3
Luis Fco Ortiz <luis.fco.om@gmail.com>
2015-11-23 17:09:45 +0100

    Subida de ficheros

---
+++ app/src/main/java/manrique/ociovertical/MainActivity.java
@@ -0,0 +1,66 @@
+package manrique.ociovertical;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Comienzo de Webview
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        // Activamos Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        // Url que carga la app (webview)Se indica la web que queremos encapsular
+        mWebView.loadUrl("http://example.com/");
+        // Forzamos el webview para que abra los enlaces internos dentro de la la APP
+        mWebView.setWebViewClient(new WebViewClient());
+        // Forzamos el webview para que abra los enlaces externos en el navegador
+        mWebView.setWebViewClient(new MyAppWebViewClient());
+        // Final Webview
+
+    } //Fin de onCreate principal funcion
+
+    @Override
+    // Detectar cuando se presiona el botón de retroceso
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
